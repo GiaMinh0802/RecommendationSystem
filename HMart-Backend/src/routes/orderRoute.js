3e159ad861c5a26cdd63d664a999d0bece843247
GiaMinh0802 <93763817+GiaMinh0802@users.noreply.github.com>
2024-06-08 10:33:32 +0700

    Update orderRoute.js

--- HMart-Backend/src/routes/orderRoute.js
+++ HMart-Backend/src/routes/orderRoute.js
@@ -3,16 +3,13 @@ const router = express.Router()
 const { getOrders, getOrderById, getOrder, createOrder, updateStatusOrder, createPayment, returnPayment, statisticOrder } = require('../controllers/orderController')
 const { jwtMiddleware } = require('../middlewares/jwtMiddleware')
 
+router.post('/create_payment_url', createPayment)
+router.get('/vnpay_return', returnPayment)
 router.get('/statistic', statisticOrder)
-
 router.get('/all', getOrders)
 router.get('/:id', getOrderById)
-
 router.get('/', jwtMiddleware, getOrder)
 router.post('/', jwtMiddleware, createOrder)
 router.put('/', jwtMiddleware, updateStatusOrder)
-
-router.post('/create_payment_url', createPayment)
-router.get('/vnpay_return', returnPayment)
 
 module.exports = router
